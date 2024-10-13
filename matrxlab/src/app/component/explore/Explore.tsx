2b4b54df7015c4dd84c0ffdcaa316ec92ceacaa2
Aman Bind <amanbind36@gmail.com>
2024-10-14 02:13:23 +0530

    explore our Dapp

--- matrxlab/src/app/component/explore/Explore.tsx
+++ matrxlab/src/app/component/explore/Explore.tsx
@@ -9,8 +9,10 @@ const Explore = () => {
             <p className='exploretext'> Explore Our <span style={{color:"rgba(98, 240, 254, 1)"}}>dApp</span></p>
             <p className='texteathai'>EthAi is an AI-powered dApp designed to help traders make smarter, data-driven decisions. By tracking smart money flows, monitoring key wallets, and providing real-time market insights, EthAi empowers users to stay ahead of trends. The platform offers intuitive AI features for asset recommendations, market analysis, and personalized crypto Q&A, making it the ultimate tool for both novice and experienced traders.</p>
             </div>
-            
-           <button className='explorebutton'>Open dApp</button>
+            <div style={{margin:"auto"}}>
+            <button className='explorebutton'>Open dApp</button>
+
+            </div>
         </div>
     </div>
   )
